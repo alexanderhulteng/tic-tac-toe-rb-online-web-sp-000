0110c9a084d089b3c2a2941ac9860c68f02f0614
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-08-04 22:05:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player)
-  board[index] = current_player
+  board[index] = current_player(board)
 end
 
 def valid_move?(board, index)
